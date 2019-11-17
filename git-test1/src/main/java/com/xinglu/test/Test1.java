41ff6c6b953643146f3af176c4adbf4fabb730af
xinglu <1062809162@qq.com>
2019-11-17 20:31:29 +0800

    第一次提交git-test1

---
+++ git-test1/src/main/java/com/xinglu/test/Test1.java
@@ -0,0 +1,12 @@
+package com.xinglu.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+    }
+}
